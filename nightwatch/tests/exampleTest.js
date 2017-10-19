b89486bc91463672b03b946d4cd21080eff4a040
AJ Larson <aj.larson@devmounta.in>
2017-10-19 09:44:52 -0600

    updated tests

--- nightwatch/tests/exampleTest.js
+++ nightwatch/tests/exampleTest.js
@@ -10,40 +10,10 @@ module.exports = {
         browser.end()
     },
     'I can put data in the fields and generate a result' : browser => {
-            //set the transaction from your data file 
-        let transaction = data.transactions.minimumEntry
-            //send the fields & data to input
-        functions.inputSet(selectors.fields, transaction.fields, browser)
-        browser
-            //submit
-            .click(selectors.buttons.submit)
-            .pause(100)
-            //expect header to be right
-            .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
-            //expect error list to be empty
-        browser.expect.element(selectors.messages.errorList).text.to.equal('')
-            //expect query title to be right
-        browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
-            //expect assembled query to be right
-        browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+        functions.test(true, data.transactions.minimumEntry, browser)
     },
     'I can put only the oln and get an error about other fields' : browser => {
-            //set the transaction from your data file
-        let transaction = data.transactions.olnOnly
-            //send the fields & data to input
-        functions.inputSet(selectors.fields, transaction.fields, browser)
-        browser
-            //submit
-            .click(selectors.buttons.submit)
-            .pause(100)
-            //expect header to be right
-            .expect.element(selectors.messages.header).text.to.equal(transaction.results.header)
-            //expect error list to contain all the right errors
-        functions.messagesCheck(selectors.messages.errorList, transaction.results.errorList, browser)
-            //expect query title to be right
-        browser.expect.element(selectors.messages.queryTitle).text.to.equal(transaction.results.queryTitle)
-            //expect assembled query to be right
-        browser.expect.element(selectors.messages.assembledQuery).text.to.equal(transaction.results.assembledQuery)
+        functions.test(false, data.transactions.olnOnly, browser)
     },
     'I can enter oln + oly and get a message that I need all the oln fields' : browser => {
             //set the transaction from your data file
